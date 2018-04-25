f76eaba2bc1539467c105145f1669b0681d0512e
Tran Thanh Tung <tttung1996@gmail.com>
2018-04-25 18:15:38 +0700

    include library lcd_driver and edit function main to test it

--- main.c
+++ main.c
@@ -11,9 +11,22 @@
 #include <inc/hw_memmap.h>
 #include <inc/tm4c123gh6pm.h>
 #include "config.h"
+#include "lcd_driver.h"
 
 int main(void){
 	System_Init();
+	GPIO_Init();
+	LCD_init();
+	LCD_reset();
+	LCD_begin();
+	setRotation(0);
 	while(1) {
+		drawPixel(10, 20, RED);
+		drawPixel(11, 20, RED);
+		drawPixel(11, 21, RED);
+		drawPixel(10, 21, RED);
+
+		drawFastHLine(50, 50, 100, RED);
+		SysTick_Wait1ms(50);
 	}
 }
